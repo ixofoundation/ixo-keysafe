4ad04358ce81f174fd9da1632321a62a2caf9ca2
sourcecodesmith <herman@sourcecodesmith.com>
2018-05-18 11:28:31 +0200

    removed setup of Raven error reporting

--- app/scripts/ui.js
+++ app/scripts/ui.js
@@ -21,7 +21,7 @@ async function start() {
 
   // setup sentry error reporting
   const release = global.platform.getVersion()
-  setupRaven({ release })
+  // setupRaven({ release })
 
   // inject css
   // const css = MetaMaskUiCss()
@@ -77,7 +77,7 @@ async function start() {
   }
 
   function displayCriticalError (err) {
-    container.innerHTML = '<div class="critical-error">The MetaMask app failed to load: please open and close MetaMask again to restart.</div>'
+    container.innerHTML = '<div class="critical-error">The IxoCredentialManager app failed to load: please open and close it again to restart.</div>'
     container.style.height = '80px'
     log.error(err.stack)
     throw err
